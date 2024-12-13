6ff917daac848e40c2c2f1986ab4a111a306e486
Ni0301 <lockdess7@gmail.com>
2024-12-13 09:34:03 +0100

    TP1

---
+++ Programme5.py
@@ -0,0 +1,5 @@
+import markdown
+from Programme3 import extraire_seances_r107, extraire_evenements, convertir_evenement_csv
+from Programme4 import compter_seances_par_mois, creer_graphique
+
+def creer_markdown(seances, compte_mois):
