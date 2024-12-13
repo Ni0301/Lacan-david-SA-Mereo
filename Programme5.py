f8281271b59418cbb4ed330112fb4ccc3dcda835
Ni0301 <lockdess7@gmail.com>
2024-12-13 09:50:11 +0100

    TP

--- Programme5.py
+++ Programme5.py
@@ -3,3 +3,7 @@ from Programme3 import extraire_seances_r107, extraire_evenements, convertir_eve
 from Programme4 import compter_seances_par_mois, creer_graphique
 
 def creer_markdown(seances, compte_mois):
+    md_content = """
+| Date | Durée | Type |
+|------|--------|------|
+"""
